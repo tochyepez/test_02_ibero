1f9ece47bd7b8b06ec560bcf9d5119289162ab09
zhenx <zhenx@something.com>
2018-05-06 17:10:39 -0500

    resolucion del segundo ejercicio

---
+++ analisis.py
@@ -0,0 +1,161 @@
+#!env/bin/python
+
+import random
+
+import csv
+import sys
+
+import spacy
+from spacy.lang.en import English
+
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+
+import gensim
+from gensim import corpora
+
+import _pickle as cPickle
+
+import pyLDAvis.gensim
+
+
+spacy.load('en')
+parser = English()
+
+
+def tokenize(text):
+    lda_tokens = []
+    tokens = parser(text)
+    for token in tokens:
+        if token.orth_.isspace():
+            continue
+        elif token.like_url:
+            lda_tokens.append('URL')
+        elif token.orth_.startswith('@'):
+            lda_tokens.append('SCREEN_NAME')
+        else:
+            lda_tokens.append(token.lower_)
+    return lda_tokens
+
+
+nltk.download('wordnet')
+
+
+def get_lemma(word):
+    lemma = wn.morphy(word)
+    if lemma is None:
+        return word
+    else:
+        return lemma
+
+
+def get_lemma2(word):
+    return WordNetLemmatizer().lemmatize(word)
+
+
+for w in ['dogs', 'ran', 'discouraged']:
+    print(w, get_lemma(w), get_lemma2(w))
+
+
+nltk.download('stopwords')
+en_stop = set(nltk.corpus.stopwords.words('english'))
+
+
+def prepare_text_for_lda(text):
+    tokens = tokenize(text)
+    tokens = [token for token in tokens if len(token) > 4]
+    tokens = [token for token in tokens if token not in en_stop]
+    tokens = [get_lemma(token) for token in tokens]
+    return tokens
+
+
+maxInt = sys.maxsize
+decrement = True
+
+while decrement:
+    # decrease the maxInt value by factor 10
+    # as long as the OverflowError occurs.
+
+    decrement = False
+    try:
+        csv.field_size_limit(maxInt)
+        print("[+]Int Max: {0}".format(maxInt))
+    except OverflowError:
+        maxInt = int(maxInt/10)
+        print("[-]Int max : {0}".format(maxInt))
+        decrement = True
+
+
+text_data = []
+with open('data/articles2.csv') as fh:
+    csvreader = csv.reader(fh)
+    for row in csvreader:
+        title = row[2].strip().split('-')[0]
+        content = row[9].strip()
+        total = ",".join([title, content])
+        tokens = prepare_text_for_lda(total)
+        if random.random() > 0.99:
+            print(tokens)
+            text_data.append(tokens)
+
+
+#    for line in f:
+#        tokens = prepare_text_for_lda(line)
+#        if random.random() > .99:
+#            print(tokens)
+#            text_data.append(tokens)
+
+dictionary = corpora.Dictionary(text_data)
+
+corpus = [dictionary.doc2bow(text) for text in text_data]
+
+cPickle.dump(corpus, open('corpus.pkl', 'wb'))
+dictionary.save('dictionary.gensim')
+
+NUM_TOPICS = 5
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=15)
+ldamodel.save('model5.gensim')
+
+
+topics = ldamodel.print_topics(num_words=4)
+for topic in topics:
+    print(topic)
+
+
+new_doc = 'Practical Bayesian Optimization of Machine Learning Algorithms'
+new_doc = prepare_text_for_lda(new_doc)
+new_doc_bow = dictionary.doc2bow(new_doc)
+print(new_doc_bow)
+print(ldamodel.get_document_topics(new_doc_bow))
+
+
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 3, id2word=dictionary, passes=15)
+ldamodel.save('model3.gensim')
+topics = ldamodel.print_topics(num_words=4)
+for topic in topics:
+    print(topic)
+
+
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 10, id2word=dictionary, passes=15)
+ldamodel.save('model10.gensim')
+topics = ldamodel.print_topics(num_words=4)
+for topic in topics:
+    print(topic)
+
+
+dictionary = gensim.corpora.Dictionary.load('dictionary.gensim')
+corpus = cPickle.load(open('corpus.pkl', 'rb'))
+lda = gensim.models.ldamodel.LdaModel.load('model5.gensim')
+
+lda_display = pyLDAvis.gensim.prepare(lda, corpus, dictionary, sort_topics=False)
+pyLDAvis.display(lda_display)
+
+
+lda3 = gensim.models.ldamodel.LdaModel.load('model3.gensim')
+lda_display3 = pyLDAvis.gensim.prepare(lda3, corpus, dictionary, sort_topics=False)
+pyLDAvis.display(lda_display3)
+
+lda10 = gensim.models.ldamodel.LdaModel.load('model10.gensim')
+lda_display10 = pyLDAvis.gensim.prepare(lda10, corpus, dictionary, sort_topics=False)
+pyLDAvis.display(lda_display10)
